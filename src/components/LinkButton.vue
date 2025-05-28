76c8a8c31a596816058f30aea96017934c1f2a66
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2025-05-28 20:34:10 +0200

    initial commit

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <a
+        :href="url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="block w-full text-center py-3 rounded-lg bg-indigo-600 hover:bg-indigo-700 transition-colors text-white font-semibold shadow-md"
+    >
+        {{ label }}
+    </a>
+</template>
+
+<script setup>
+defineProps({
+    label: String,
+    url: String,
+});
+</script>
+
+<style scoped>
+/* Możesz dodać dodatkowe style, jeśli chcesz */
+</style>
